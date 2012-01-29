aba77673596d74d5de3baba5b816679925beb106
Satrya Bima <satryabima24@gmail.com>
2012-01-29 23:15:40 +0700

    updated

--- functions/rumputhijau_theme_enqueue.php
+++ functions/rumputhijau_theme_enqueue.php
@@ -60,8 +60,8 @@ function init_styles() {
 ################################################################################
 
 function footer_scripts() { ?>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js" type="text/javascript"></script>')</script>
 
 <!--[if lt IE 7 ]>
 	<script src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script>
